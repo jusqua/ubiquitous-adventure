f8db6daeed723a5359666ba920d8730de1e23c13
Ádrian Gama <adrian.gama.se@gmail.com>
2025-11-26 16:43:24 -0300

    Rename particle attributes

--- src/scenes/particle.lua
+++ src/scenes/particle.lua
@@ -2,16 +2,16 @@ local Scene = require "src.primitives.scene"
 
 ---@class Particle : Scene
 ---@field color [number, number, number, number]
----@field _original_size number
----@field size number
+---@field original_size number
+---@field current_size number
 ---@field decay_factor number
 local Particle = Scene:inherit("Particle")
 
 function Particle.new()
     local self = setmetatable(Particle.super.new(), { __index = Particle })
     self.color = { math.random(127, 255) / 255, math.random(127, 255) / 255, math.random(127, 255) / 255, 1 }
-    self._original_size = math.random(5, 20)
-    self.size = self._original_size
+    self.original_size = math.random(5, 20)
+    self.current_size = self.original_size
     self.decay_factor = math.random(1, 3)
     self.x = math.random(0, love.graphics.getWidth())
     self.y = math.random(0, love.graphics.getHeight())
@@ -19,16 +19,16 @@ function Particle.new()
 end
 
 function Particle:update(dt)
-    self.size = self.size - self.decay_factor * dt
-    if self.size <= 0 then
+    self.current_size = self.current_size - self.decay_factor * dt
+    if self.current_size <= 0 then
         self:destroy()
     end
 end
 
 function Particle:draw()
-    self.color[4] = self.size / self._original_size
+    self.color[4] = self.current_size / self.original_size
     love.graphics.setColor(self.color)
-    love.graphics.circle("fill", self.x - self.size / 2, self.y - self.size / 2, self.size)
+    love.graphics.circle("fill", self.x - self.current_size / 2, self.y - self.current_size / 2, self.current_size)
 end
 
 return Particle
