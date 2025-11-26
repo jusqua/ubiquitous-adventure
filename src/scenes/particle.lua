1c5a5c7231461b74f234bd1b5ea42eb7a998b101
Ádrian Gama <adrian.gama.se@gmail.com>
2025-11-26 00:23:16 -0300

    Fix particle fading

--- src/scenes/particle.lua
+++ src/scenes/particle.lua
@@ -1,7 +1,7 @@
 local Scene = require "src.primitives.scene"
 
 ---@class Particle : Scene
----@field color [number, number, number]
+---@field color [number, number, number, number]
 ---@field _original_size number
 ---@field size number
 ---@field decay_factor number
@@ -9,7 +9,7 @@ local Particle = Scene:inherits()
 
 function Particle.new()
     local self = Particle.super.new()
-    self.color = { math.random(127, 255) / 255, math.random(127, 255) / 255, math.random(127, 255) / 255 }
+    self.color = { math.random(127, 255) / 255, math.random(127, 255) / 255, math.random(127, 255) / 255, 1 }
     self._original_size = math.random(5, 20)
     self.size = self._original_size
     self.decay_factor = math.random(1, 3)
@@ -26,7 +26,8 @@ function Particle:update(dt)
 end
 
 function Particle:draw()
-    love.graphics.setColor(self.color, self.size / self._original_size)
+    self.color[4] = self.size / self._original_size
+    love.graphics.setColor(self.color)
     love.graphics.circle("fill", self.x - self.size / 2, self.y - self.size / 2, self.size)
 end
 
